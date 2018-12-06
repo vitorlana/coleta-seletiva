441f77670102f3d9e60d89455c35456287248ac1
vitorlana <vitorhugolana@hotmail.com>
2018-12-05 23:40:37 -0200

    apagar material

--- codigo/material/metal.cpp
+++
@@ -1,9 +0,0 @@
-#include "metal.h"
-Metal::Metal(float quantidade_em_quilos, std::string breve_descricao)
-:Material ("Metal", quantidade_em_quilos, breve_descricao){}
-
-void Metal::modo_de_armazenamento(){
-    std::cout<<"SEU MATERIAL FOI CADASTRADO COM SUCESSO!"<<std::endl;
-    std::cout<<"MODO DE ARMAZENAMENTO: "<<std::endl;
-    std::cout<< "Colocar o material limpo e seco em caixas ou sacolas" << std::endl << "Sinalizar se houver algum risco de corte" << std:: endl<< std:: endl;
-}
--- codigo/material/papel.cpp
+++
@@ -1,9 +0,0 @@
-#include "papel.h"
-Papel::Papel(float quantidade_em_quilos, std::string breve_descricao)
-:Material ("Papel", quantidade_em_quilos, breve_descricao){}
-
-void Papel::modo_de_armazenamento(){
-    std::cout<<"SEU MATERIAL FOI CADASTRADO COM SUCESSO!"<<std::endl;
-    std::cout<<"MODO DE ARMAZENAMENTO: "<<std::endl;
-    std::cout<<"Colocar o material limpo e seco em caixas ou sacolas"<< std:: endl<< std:: endl;
-}
