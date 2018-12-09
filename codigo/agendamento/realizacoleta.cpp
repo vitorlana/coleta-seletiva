7779d74ac161ff7e44f6a8cd0dd647a4ad2f5627
vitorlana <vitorhugolana@hotmail.com>
2018-12-09 21:38:48 -0200

    coleta agendamento

--- codigo/agendamento/realizacoleta.cpp
+++ codigo/agendamento/realizacoleta.cpp
@@ -31,7 +31,6 @@ void Realiza_Coleta::Imprime_Coletas_Realizadas(std::vector<Coleta*> coletas_dis
 
 void Realiza_Coleta::Realizar_Coleta(std::vector<Coleta*> coletas_disponiveis, Pessoa *usuario,CadastroMaterial *materiais){
     int n_coleta;
-    Coleta *aux;
     system("cls||clear");
     std::cout << "ESSAS SÃO AS COLETAS QUE VOCE PODE REALIZAR" << '\n';
     std::cout << "ESCOLHA UMA DAS COLETAS INDICANDO O NUMERO NA FRENTE DELAS:" << '\n';
@@ -39,9 +38,9 @@ void Realiza_Coleta::Realizar_Coleta(std::vector<Coleta*> coletas_disponiveis, P
     std::cout << "INSIRA O NUMERO DA COLETA QUE QUER REALIZAR:" << '\n';
     std::cin >> n_coleta;
     //Recebe_vector(materiais,get_coleta(coletas_disponiveis,coletas_disponiveis.at(n_coleta)->get_posicao_vetor()).get_doador());
-    *aux = get_coleta(coletas_disponiveis,n_coleta);
-    materiais->excluir_material(aux->get_posicao_vetor(), aux->get_doador());
-    aux->set_coleta_realizada();
+    //Coleta *aux = get_coleta(coletas_disponiveis,n_coleta);
+    materiais->excluir_material(coletas_disponiveis.at(n_coleta)->get_posicao_vetor(), coletas_disponiveis.at(n_coleta)->get_doador());
+    coletas_disponiveis.at(n_coleta)->set_coleta_realizada();
     //vector_materiais.erase(vector_materiais.begin()+n_coleta);
     std::cout << "COLETA REALIZDA COM SUCESSO" << '\n';
 }
